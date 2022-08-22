8c2c430cf5b356782ba7e9ac88d9cfa6e2523f37
Edmilson <chvez4012@gmail.com>
2022-08-22 03:00:59 -0500

    v.2

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './components/App.vue';
-
-createApp(App).mount("#app")
-/*
-new createApp({
-    render: h => h(App)
-}).$mount('#app');*/
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors")
 
 mongoose.connect('mongodb://localhost/hotel-database')
 .then(db => console.log("BD CONNECT"))
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors())
 
 
 app.use("/api/formulario",require("./routes/tasks"))
